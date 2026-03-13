93e057dbee7e4a58ae4f4549c49ec618ffa9b8e0
ztc0118 <996599034@qq.com>
2026-03-13 05:42:41 +0000

    修改架构

--- smarttable/settings.py
+++ smarttable/settings.py
@@ -75,6 +75,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
